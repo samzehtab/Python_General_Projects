642f98240fcfbea2c27b0409f4e40b44bee7178a
samzehtab <samzehtab7@gmail.com>
2024-12-09 11:01:59 +0330

    Find_Hypot

---
+++ Script31_Book_S5_Exe10_find_hypot_Lec08.py
@@ -0,0 +1,12 @@
+#In the name of God
+#Mohammad Hossein Zehtab
+#python-evens-17
+#S5 Book Exc10: Find Hypot Function
+
+def find_hypot(base: float, prepen: float) -> float:
+    '''
+    Finding Hypotenuse
+    '''
+    addsq = base ** 2 + prepen ** 2
+    hypot = addsq ** 0.5
+    return hypot
